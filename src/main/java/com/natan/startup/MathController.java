bcbc62cc6021499e15745e878b3026cd4c6e10b9
Natan Oliveira <dev.natanoliveira@gmail.com>
2024-05-16 15:48:08 -0300

    feat: add division method to math controller

--- src/main/java/com/natan/startup/MathController.java
+++ src/main/java/com/natan/startup/MathController.java
@@ -28,6 +28,15 @@ public class MathController {
         return convertToDouble(numberOne) - convertToDouble(numberTwo);
     }
 
+    @RequestMapping(value = "/div/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+    public Double div(@PathVariable(value = "numberOne") String numberOne,
+                      @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo))
+            throw new UnsuportedMathOperationException("Please set a numeric value");
+
+        return (convertToDouble(numberOne) / convertToDouble(numberTwo));
+    }
+
     private Double convertToDouble(String strNumber) {
         if (strNumber == null ) return 0D;
         String number = strNumber.replaceAll(",", ".");
