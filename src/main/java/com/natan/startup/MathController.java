870b3749454110bd4613142ef13d55a6c2d8dd2a
Natan Oliveira <dev.natanoliveira@gmail.com>
2024-05-16 15:36:25 -0300

    feat: add sub operation method to math controller

--- src/main/java/com/natan/startup/MathController.java
+++ src/main/java/com/natan/startup/MathController.java
@@ -9,15 +9,23 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class MathController {
     private  final AtomicLong counter = new AtomicLong();
-    @RequestMapping(value = "/sum/{numberOne}/{numberTwo}",
-    method=RequestMethod.GET)
-    public Double sum(
-            @PathVariable(value="numberOne") String numberOne,
-            @PathVariable(value="numberTwo") String numberTwo
-    ) throws Exception {
+
+    @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+    public Double sum(@PathVariable(value="numberOne") String numberOne,
+                      @PathVariable(value="numberTwo") String numberTwo) throws Exception {
         if(!isNumeric(numberOne) || !isNumeric(numberTwo))
             throw new UnsuportedMathOperationException("Please set a numeric value");
+
         return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    @RequestMapping(value = "/sub/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+    public Double sub(@PathVariable(value = "numberOne") String numberOne,
+                      @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo))
+            throw new UnsuportedMathOperationException("Please set a numeric value");
+
+        return convertToDouble(numberOne) - convertToDouble(numberTwo);
     }
 
     private Double convertToDouble(String strNumber) {
