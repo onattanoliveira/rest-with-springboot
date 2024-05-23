99690d2addae600e6d699dc46568d56b82311bed
Natan Oliveira <dev.natanoliveira@gmail.com>
2024-05-23 11:18:05 -0300

    fix: api url

--- src/main/java/com/natan/startup/controller/v1/PersonController.java
+++ src/main/java/com/natan/startup/controller/v1/PersonController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/person")
+@RequestMapping("/api/v1/person")
 public class PersonController {
 
     @Autowired
--- src/main/java/com/natan/startup/controller/v2/PersonControllerV2.java
+++ src/main/java/com/natan/startup/controller/v2/PersonControllerV2.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v2/person")
+@RequestMapping("/api/v2/person")
 public class PersonControllerV2 {
 
     @Autowired
